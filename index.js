066b20d330b8675f8546a1e3f1bfd80a3e8c55f7
Mikaela Rodriguez <mikrdgz@gmail.com>
2019-08-11 17:42:56 -0500

    added mongoose connection to cloud db

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ const express = require("express");
 let productsRoutes = require("./routes/products");
 let vehicleRoutes = require("./routes/vehicle");
 let contactsRoutes = require("./routes/contacts");
+let mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+
+const dbUrl = 'mongodb+srv://mika:kgubf6gDv63TSgV@cluster0-kro0j.mongodb.net/test?retryWrites=true';
+
+mongoose.connect(dbUrl, {useNewUrlParser: true});
+
+const Cat = mongoose.model('Cat', { name: String });
+
+const kitty = new Cat({ name: 'Theodora' });
+kitty.save().then(() => console.log('meow'));
 
 //express
 const app = express();
